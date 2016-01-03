f8b65d9dcf747504371a5f8fc539f71bca2e534c
Finnishandy <finnishnandy@gmail.com>
2016-01-03 19:22:30 +0000

    woo

--- server.js
+++ server.js
@@ -3,5 +3,5 @@
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+connect().use(serveStatic(__dirname)).listen(80);
 console.log("Server running. Browse to http://localhost:8080");
